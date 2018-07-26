25687e92106f1b75cf8a4c7abbf938c66e980b55
HareInWeed <34718939+HareInWeed@users.noreply.github.com>
2018-07-26 14:42:51 +0800

    add verifyExchanges

--- main.py
+++ main.py
@@ -1,6 +1,51 @@
 from exchange import *
 import time
 
+def verifyExchanges(
+        exchangesData: {
+            'exchangeName': (
+                [('buyPriceArgN, handling fee considered, from HIGH to LOW',
+                  'buyTotalAmountN'), ],
+                [('sellPriceArgN, handling fee considered, from LOW to HIGH',
+                  'sellTotalAmountN'), ]
+            ),
+        }
+    ) -> [
+        (
+            ('buyExchange: the exchange to sell assets',
+            'sellExchange: the exchange to buy assets'),
+            ('level',
+            'amount'),
+        ),
+    ]:
+
+    # index enumeration for taking buy/sell data from exchange tuple
+    BUY, SELL = range(2)
+
+    # index enumeration for taking price/amount data from price/amount tuple
+    PRICE, AMOUNT = range(2)
+
+    validExPairs = []
+    for buyExName, buyEx in exchangesData.items():
+        for sellExName, sellEx in exchangesData.items():
+            if buyExName == sellExName: continue
+            print('{} {}'.format(buyExName, sellExName))
+            if buyEx[BUY][0][PRICE] < sellEx[SELL][0][PRICE]: continue
+
+            level = 0
+            amount = 0
+
+            for i, (buy, sell) in enumerate(zip(buyEx[BUY], sellEx[SELL])):
+                print('{} {}'.format(buy[PRICE], sell[PRICE]))
+                if buy[PRICE] < sell[PRICE]:
+                    level = i - 1
+                    amount = min(buyEx[BUY][level][AMOUNT], sellEx[SELL][level][AMOUNT])
+                    break
+
+            validExPairs.append( ((buyExName, sellExName), (level, amount)) )
+
+    return validExPairs
+
 def run(exchanges, coinPairs):
     while True:
         exchangeState = dict()
@@ -22,8 +67,8 @@ def run(exchanges, coinPairs):
                 totalAmount += amount
                 sellAvg.append((total / totalAmount, totalAmount))
             exchangeState[exchange] = (buyAvg, sellAvg)
-        # TODO: find two exhanges that can trade
 
+        exchangePairs = verifyExchanges(exchangeState)
 
 if __name__ == "main":
     run(['huobi', 'gate', 'bitfinex'], ('eth', 'usdt'))
