4d72deb11190693cf16c90df0480dc603c29b573
Nouzan <clobraindie@outlook.com>
2018-08-01 23:29:00 +0800

    add Bitfinex.getOrderBook

---
+++ exchanges/bitfinex/BitfinexService.py
@@ -0,0 +1,52 @@
+from exchanges.base import ExchangeService
+from request import get
+from .bitfinex_key import ApiKey, SecretKey
+
+from twisted.internet import reactor
+
+import json
+
+class Bitfinex(ExchangeService):
+
+    def __init__(self, url, accessKey, secretKey):
+        self.__url = url
+        self.__accessKey = accessKey
+        self.__secretKey = secretKey
+
+    def getSymbol(self, pairs):
+        coin, money = pairs
+        if money.upper() == 'USDT':
+            money = 'USD'
+        return ''.join((coin, money)).upper()
+
+    def getOrderBook(self, pairs):
+        URL = "/v1/book/"
+        # print(self.__url)
+        url = self.__url + URL + self.getSymbol(pairs)
+        # print(url)
+
+        d = get(reactor, url)
+
+        def handleBody(body):
+            # print(body)
+            data = json.loads(body)
+            # print(data)
+            rawBids = data['bids']
+            rawAsks = data['asks']
+
+            bids = []
+            asks = []
+
+            for bid in rawBids:
+                bids.append([bid['price'], bid['amount']])
+
+            for ask in rawAsks:
+                asks.append([ask['price'], ask['amount']])
+
+            return [bids, asks]
+
+        d.addCallback(handleBody)
+
+        return d
+
+bitfinex = Bitfinex('https://api.bitfinex.com', ApiKey, SecretKey)
--- main.py
+++ main.py
@@ -6,8 +6,69 @@ import time
 from twisted.internet import defer
 from twisted.internet import reactor
 from request import get
+from utils import calcMean
+from exchange import FEE
 from exchanges.gateio.GateIOService import gateio
+from exchanges.bitfinex.BitfinexService import bitfinex
 
+count = 0
+
+def verifyExchanges(
+        exchangesData: {
+            'exchangeName': {
+                'avg': (
+                    [('buyPriceArgN, handling fee considered, from HIGH to LOW',
+                      'buyTotalAmountN'), ],
+                    [('sellPriceArgN, handling fee considered, from LOW to HIGH',
+                      'sellTotalAmountN'), ]
+                ),
+                'actual': (
+                    [('buyPriceArgN, handling fee considered, from HIGH to LOW',
+                      'buyTotalAmountN'), ],
+                    [('sellPriceArgN, handling fee considered, from LOW to HIGH',
+                      'sellTotalAmountN'), ]
+                ),
+            },
+        }
+    ) -> [
+        (
+            ('buyExchange: the exchange to sell assets',
+            'sellExchange: the exchange to buy assets'),
+            ('level',
+            'amount'),
+        ),
+    ]:
+
+    # index enumeration for taking buy/sell data from exchange tuple
+    BUY, SELL = range(2)
+
+    # index enumeration for taking price/amount data from price/amount tuple
+    PRICE, AMOUNT = range(2)
+
+    validExPairs = []
+    for buyExName, buyEx in exchangesData.items():
+        # print(buyEx)
+        for sellExName, sellEx in exchangesData.items():
+            if buyExName == sellExName: continue
+            if buyEx['avg'][BUY][0][PRICE] * FEE[buyExName][BUY] <= sellEx['avg'][SELL][0][PRICE] * FEE[sellExName][SELL]: continue
+
+            level = 0
+            amount = 0
+
+            # for i, (buy, sell) in enumerate(zip(buyEx['avg'][BUY], sellEx['avg'][SELL])):
+            #     # print(buy, sell)
+            #     level = i
+            #     if buy[PRICE] * FEE[buyExName][BUY] <= sell[PRICE] * FEE[sellExName][SELL]:
+            #         level = i - 1
+            #         break
+
+            amount = min(buyEx['avg'][BUY][level][AMOUNT], sellEx['avg'][SELL][level][AMOUNT])
+            buyPrice = float(buyEx['actual'][BUY][level][PRICE])
+            sellPrice = float(sellEx['actual'][SELL][level][PRICE])
+
+            validExPairs.append( ((buyExName, sellExName), (buyPrice, sellPrice), (level, amount)) )
+
+    return validExPairs
 
 def cbCondition(body1, body2):
     ob1 = json.loads(body1)
@@ -16,17 +77,44 @@ def cbCondition(body1, body2):
 
 @defer.inlineCallbacks
 def cbNext():
-    print('looping')
-    time.sleep(1)
+    global count
+    print('Loop:', count)
+    count += 1
+    # time.sleep(1)
     # d = task.deferLater(reactor, 0, main, reactor)
     try:
-        bids, asks = yield gateio.getOrderBook(('eth', 'usdt'))
-        # body2 = yield getData(reactor, b"https://api.huobi.pro/market/depth?symbol=ethusdt&type=step0")
-        # body2 = yield get(reactor, b"https://api.bitfinex.com/v1/book/ETHUSD")
-        # print(body2)
+        bids1, asks1 = yield gateio.getOrderBook(('eth', 'usdt'))
+        # print(len(bids1))
+        bids2, asks2 = yield bitfinex.getOrderBook(('eth', 'usdt'))
+        # print(len(bids2))
     except Exception as err:
         print(err)
-    print(len(bids))
+    # print(len(bids1), len(bids2))
+    try:
+        exchangeState = dict()
+
+        exchangeState['gateio'] = dict()
+        exchangeState['bitfinex'] = dict()
+
+        avgBids1 = calcMean(bids1)
+        avgAsks1 = calcMean(asks1, True)
+
+        # print(avgBids1)
+
+        exchangeState['gateio']['actual'], exchangeState['gateio']['avg'] = [bids1, asks1], [avgBids1, avgAsks1]
+
+        avgBids2 = calcMean(bids2)
+        avgAsks2 = calcMean(asks2)
+
+        # print(avgBids2)
+
+        exchangeState['bitfinex']['actual'], exchangeState['bitfinex']['avg'] = [bids2, asks2], [avgBids2, avgAsks2]
+    except Exception as err:
+        print(err)
+
+    exchangePairs = verifyExchanges(exchangeState)
+    print(exchangePairs)
+
 
     yield cbNext()
 
