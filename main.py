7a554be302b085bde1b8b8c0df761ca8d082ae96
Nouzan <clobraindie@outlook.com>
2018-08-03 14:21:53 +0800

    delete testing codes

--- main.py
+++ main.py
@@ -15,54 +15,6 @@ from exchange import OrderBooks
 
 count = 0
 
-def cbCondition(body1, body2):
-    ob1 = json.loads(body1)
-    ob2 = json.loads(body2)
-    print(ob1['asks'][0], ob2['bids'][0])
-
-@defer.inlineCallbacks
-def cbNext():
-    global count
-    print('Loop:', count)
-    count += 1
-    # time.sleep(1)
-    # d = task.deferLater(reactor, 0, main, reactor)
-    try:
-        bids1, asks1 = yield gateio.getOrderBook(('eth', 'usdt'))
-        # print(len(bids1))
-        bids2, asks2 = yield bitfinex.getOrderBook(('eth', 'usdt'))
-        # print(len(bids2))
-    except Exception as err:
-        print(err)
-    # print(len(bids1), len(bids2))
-    try:
-        exchangeState = dict()
-
-        exchangeState['gateio'] = dict()
-        exchangeState['bitfinex'] = dict()
-
-        avgBids1 = calcMean(bids1)
-        avgAsks1 = calcMean(asks1, True)
-
-        # print(avgBids1)
-
-        exchangeState['gateio']['actual'], exchangeState['gateio']['avg'] = [bids1, asks1], [avgBids1, avgAsks1]
-
-        avgBids2 = calcMean(bids2)
-        avgAsks2 = calcMean(asks2)
-
-        # print(avgBids2)
-
-        exchangeState['bitfinex']['actual'], exchangeState['bitfinex']['avg'] = [bids2, asks2], [avgBids2, avgAsks2]
-    except Exception as err:
-        print(err)
-
-    exchangePairs = verifyExchanges(exchangeState)
-    print(exchangePairs)
-
-
-    yield cbNext()
-
 orderBooks = OrderBooks(['gateio', 'bitfinex'], ('eth', 'usdt'))
 orderBooks.start(reactor)
 
