50ba1d5f827cd28a8623b3ce95100ef11ce4ac0e
Nouzan.Ubu <clobraindie@outlook.com>
2018-08-14 00:23:01 +0800

    fix state problem

--- future.py
+++ future.py
@@ -255,163 +255,162 @@ def cbRun():
 
     print(bool(KLinesData), bool(tickerData), bool(positionData), bool(orderBookData), bool(userInfoData))
 
-    if state == 'GO':
-        # print(len(klines), ticker, position)
-        # 是否开初始单
-        if KLinesData is not None and tickerData is not None and positionData is not None:
-            total += 1
-            wait -= 1
-            print('avg wait:', wait / total)
+    # 是否开初始单
+    if state == 'GO' and KLinesData is not None and tickerData is not None and positionData is not None:
+        total += 1
+        wait -= 1
+        print('avg wait:', wait / total)
 
-            MAs = calcMAs(KLinesData, ma=30)
-            position = positionData
-            buy_amount = position['buy_amount']
-            sell_amount = position['sell_amount']
-            timestamp, ma = MAs[-1]
+        MAs = calcMAs(KLinesData, ma=30)
+        position = positionData
+        buy_amount = position['buy_amount']
+        sell_amount = position['sell_amount']
+        timestamp, ma = MAs[-1]
 
-            ticker = tickerData['last']
+        ticker = tickerData['last']
 
-            print('ticker && ma:', ticker, ma)
-            print('buy_amount && sell_amount:', buy_amount, sell_amount)
+        print('ticker && ma:', ticker, ma)
+        print('buy_amount && sell_amount:', buy_amount, sell_amount)
 
-            if ticker > ma and buy_amount == 0 and len(buys) == 0:
-                print('BUY')
-                state = 'WAIT'
-                reactor.callWhenRunning(buy)
-                # reactor.callWhenRunning(buy)
+        if ticker > ma and buy_amount == 0 and len(buys) == 0:
+            print('BUY')
+            state = 'WAIT'
+            reactor.callWhenRunning(buy)
+            # reactor.callWhenRunning(buy)
 
-            if ticker < ma and sell_amount == 0 and len(sells) == 0:
-                print('SELL')
-                state = 'WAIT'
-                reactor.callWhenRunning(sell)
+        if ticker < ma and sell_amount == 0 and len(sells) == 0:
+            print('SELL')
+            state = 'WAIT'
+            reactor.callWhenRunning(sell)
 
-        # 是否平
-        if orderBookData != None and positionData != None:
-            position = positionData
-            # print(position)
-            bids, asks = orderBookData
-            buy2, _ = bids[1]
-            sell2, _ = asks[1]
-            buy_price_avg = getAvg(buys)
-            sell_price_avg = getAvg(sells)
-            buy_amount = position['buy_amount']
-            sell_amount = position['sell_amount']
-            # print(position)
-            buy_profit = position['buy_profit_real']
-            sell_profit = position['sell_profit_real']
+    # 是否平
+    if state == 'GO' and orderBookData != None and positionData != None:
+        position = positionData
+        # print(position)
+        bids, asks = orderBookData
+        buy2, _ = bids[1]
+        sell2, _ = asks[1]
+        buy_price_avg = getAvg(buys)
+        sell_price_avg = getAvg(sells)
+        buy_amount = position['buy_amount']
+        sell_amount = position['sell_amount']
+        # print(position)
+        buy_profit = position['buy_profit_real']
+        sell_profit = position['sell_profit_real']
 
-            print('buy_price_avg && buy2:', buy_price_avg, buy2)
-            print('sell_price_avg && sell2:', sell_price_avg, sell2)
+        print('buy_price_avg && buy2:', buy_price_avg, buy2)
+        print('sell_price_avg && sell2:', sell_price_avg, sell2)
 
-            if buy_price_avg == 0:
-                buyRate = 0
-            else:
-                buyRate = (buy2 - buy_price_avg) / buy_price_avg * leverage
+        if buy_price_avg == 0:
+            buyRate = 0
+        else:
+            buyRate = (buy2 - buy_price_avg) / buy_price_avg * leverage
 
-            if sell_price_avg == 0:
-                sellRate = 0
-            else:
-                sellRate = (sell_price_avg - sell2) / sell_price_avg * leverage
+        if sell_price_avg == 0:
+            sellRate = 0
+        else:
+            sellRate = (sell_price_avg - sell2) / sell_price_avg * leverage
 
-            print('buyRate && sellRate:', buyRate, sellRate)
+        print('buyRate && sellRate:', buyRate, sellRate)
 
-            if buyRate >= 0.03 and buy_amount != 0:
-                print('BUYP')
-                state = 'WAIT'
-                reactor.callWhenRunning(buyp, amount=buy_amount, price=str(buy2))
+        if buyRate >= 0.03 and buy_amount != 0:
+            print('BUYP')
+            state = 'WAIT'
+            reactor.callWhenRunning(buyp, amount=buy_amount, price=str(buy2))
 
 
-            if sellRate >= 0.03 and sell_amount != 0:
-                print('SELLP')
-                state = 'WAIT'
-                reactor.callWhenRunning(sellp, amount=sell_amount, price=str(sell2))
+        if sellRate >= 0.03 and sell_amount != 0:
+            print('SELLP')
+            state = 'WAIT'
+            reactor.callWhenRunning(sellp, amount=sell_amount, price=str(sell2))
 
 
 
-        # 布林
-        if tickerData != None and KLinesData != None and positionData != None:
-            ticker = tickerData['last']
-            KLines = KLinesData
-            position = positionData
-            buy_amount = position['buy_amount']
-            sell_amount = position['sell_amount']
+    # 布林
+    if state == 'GO' and tickerData is not None and KLinesData is not None and positionData is not None:
+        ticker = tickerData['last']
+        KLines = KLinesData
+        position = positionData
+        buy_amount = position['buy_amount']
+        sell_amount = position['sell_amount']
 
-            Bolls = calcBolls(KLines)
-            klines = KLines[-3:]
-            bolls = Bolls[-3:]
-            llk, lk, k = klines
-            llb, lb, b = bolls
-            _, _, _, _, llk_close, _, _ = llk
-            _, _, _, _, lk_close, _, _ = lk
-            _, llb_u, _, llb_d = llb
-            _, lb_u, _, lb_d = lb
-            _, b_u, _, b_d = b
-            if llk_close > llb_d and lk_close < lb_d:
-                print('BUYBOLL')
-                if buy_amount > 0:
-                    buy_price_last, buy_amount_last = buys[-1]
-                    if (buy_price_last - ticker) / buy_price_last >= 0.005:
-                        if len(buys) < 6:
-                            buy_amount_new = buy_amount_last * 1.618
-                        else:
-                            buy_amount_new = buy_amount_last
-                        print('BUY', buy_amount_new)
-                        state = 'WAIT'
-                        reactor.callWhenRunning(buy, amount=buy_amount_new)
+        Bolls = calcBolls(KLines)
+        klines = KLines[-3:]
+        bolls = Bolls[-3:]
+        llk, lk, k = klines
+        llb, lb, b = bolls
+        _, _, _, _, llk_close, _, _ = llk
+        _, _, _, _, lk_close, _, _ = lk
+        _, llb_u, _, llb_d = llb
+        _, lb_u, _, lb_d = lb
+        _, b_u, _, b_d = b
+        if llk_close > llb_d and lk_close < lb_d:
+            print('BUYBOLL')
+            if buy_amount > 0:
+                buy_price_last, buy_amount_last = buys[-1]
+                if (buy_price_last - ticker) / buy_price_last >= 0.005:
+                    if len(buys) < 6:
+                        buy_amount_new = buy_amount_last * 1.618
+                    else:
+                        buy_amount_new = buy_amount_last
+                    print('BUY', buy_amount_new)
+                    state = 'WAIT'
+                    reactor.callWhenRunning(buy, amount=buy_amount_new)
 
-            if llk_close < llb_u and lk_close > lb_u:
-                print('SELLBOLL')
-                if sell_amount > 0:
-                    sell_price_last, sell_amount_last = sells[-1]
-                    if (ticker - sell_price_last) / sell_price_last >= 0.005:
-                        if len(sells) < 6:
-                            sell_amount_new = sell_amount_last * 1.618
-                        else:
-                            sell_amount_new = sell_amount_last
-                        print('SELL', sell_amount_new)
-                        state = 'WAIT'
-                        reactor.callWhenRunning(sell, amount=sell_amount_new)
-        if userInfoData is not None and positionData is not None:
-            # 止损
-            account_rights = userInfoData['account_rights']
-            keep_deposit = userInfoData['keep_deposit']
-            profit_real = userInfoData['profit_real']
-            profit_unreal = userInfoData['profit_unreal']
+        if llk_close < llb_u and lk_close > lb_u:
+            print('SELLBOLL')
+            if sell_amount > 0:
+                sell_price_last, sell_amount_last = sells[-1]
+                if (ticker - sell_price_last) / sell_price_last >= 0.005:
+                    if len(sells) < 6:
+                        sell_amount_new = sell_amount_last * 1.618
+                    else:
+                        sell_amount_new = sell_amount_last
+                    print('SELL', sell_amount_new)
+                    state = 'WAIT'
+                    reactor.callWhenRunning(sell, amount=sell_amount_new)
 
-            position = positionData
-            buy_amount = position['buy_amount']
-            sell_amount = position['sell_amount']
+    if state == 'GO' and userInfoData is not None and positionData is not None:
+        # 止损
+        account_rights = userInfoData['account_rights']
+        keep_deposit = userInfoData['keep_deposit']
+        profit_real = userInfoData['profit_real']
+        profit_unreal = userInfoData['profit_unreal']
 
-            print('account_rights && keep_deposit:', account_rights, keep_deposit)
-            print('profit_real && profit_unreal:', profit_real, profit_unreal)
+        position = positionData
+        buy_amount = position['buy_amount']
+        sell_amount = position['sell_amount']
 
-            if account_rights > maxRight:
-                maxRight = account_rights
-            print('maxRight:', maxRight)
+        print('account_rights && keep_deposit:', account_rights, keep_deposit)
+        print('profit_real && profit_unreal:', profit_real, profit_unreal)
 
-            if -(profit_real + profit_unreal) > 0.5 * maxRight and buy_amount > 0:
-                print('PPP')
-                state = 'PPP'
-                reactor.callWhenRunning(buyp, amount=buy_amount, sellAmount=sell_amount)
+        if account_rights > maxRight:
+            maxRight = account_rights
+        print('maxRight:', maxRight)
 
-        if userInfoData is not None:
-            account_rights = userInfoData['account_rights']
-            accountRight = account_rights
-            if maxRightEveryPeriod != 0.0:
-                drawdown = (maxRightEveryPeriod - account_rights) / maxRightEveryPeriod
-            else:
-                drawdown = 0.0
+        if -(profit_real + profit_unreal) > 0.5 * maxRight and buy_amount > 0:
+            print('PPP')
+            state = 'PPP'
+            reactor.callWhenRunning(buyp, amount=buy_amount, sellAmount=sell_amount)
 
-            if drawdown > maxDrawdown:
-                maxDrawdown = drawdown
-            print('maxREP && maxDD:', maxRightEveryPeriod, maxDrawdown)
+    if userInfoData is not None:
+        account_rights = userInfoData['account_rights']
+        accountRight = account_rights
+        if maxRightEveryPeriod != 0.0:
+            drawdown = (maxRightEveryPeriod - account_rights) / maxRightEveryPeriod
+        else:
+            drawdown = 0.0
+
+        if drawdown > maxDrawdown:
+            maxDrawdown = drawdown
+        print('maxREP && maxDD:', maxRightEveryPeriod, maxDrawdown)
 
 
-        if count % 60 == 0:
-            maxRightEveryPeriod = accountRight
-            staFile = open('okex_' + coin, 'a+')
-            staFile.write("%d,%f\n" % (count, maxDrawdown))
-            staFile.close()
+    if count % 60 == 0:
+        maxRightEveryPeriod = accountRight
+        staFile = open('okex_' + coin, 'a+')
+        staFile.write("%d,%f\n" % (count, maxDrawdown))
+        staFile.close()
 
 
 
