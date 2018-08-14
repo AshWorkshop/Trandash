f01bf9eaac924ca82b3094353036f20e72d9ff7d
Nouzan.Ubu <clobraindie@outlook.com>
2018-08-14 15:24:37 +0800

    clean print

--- exchanges/bitfinex/BitfinexService.py
+++ exchanges/bitfinex/BitfinexService.py
@@ -324,7 +324,6 @@ class Bitfinex(ExchangeService):
         URL = "/v2/ticker/"
         symbol = self.getSymbol(pairs)
         url = self.__url + URL + 't' + symbol
-        print(url)
         headers = {'User-Agent': ['Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.71 Safari/537.36']}
         d = get(
             reactor,
