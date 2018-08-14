e367b4db6f4adaf7e76f6166259b739eb25eb095
Nouzan.Ubu <clobraindie@outlook.com>
2018-08-14 16:44:01 +0800

    base control

--- bitfinexRobot.py
+++ bitfinexRobot.py
@@ -18,18 +18,70 @@ else:
 
 class BitfinexRobot(Robot):
 
+    def init(self):
+        self.data['buys'] = []
+        self.data['sells'] = []
+        self.data['buyp'] = None
+        self.data['sellp'] = None
+
+        self.state = 'run'
+
     def run(self):
         cycleData = self.data['cycleData']
-        klines = cycleData['klines']
+        KLines = cycleData['klines']
         ticker = cycleData['ticker']
         catch = False
 
-        if not catch and klines is not None and ticker is not None:
+        if not catch and KLines is not None and ticker is not None:
+            buys = self.data['buys'] # 已成交的买单，相当于开多单
+            sells = self.data['sells'] # 已成交的卖单，相当于开空单
+
             catch = True
-            MAs = calcMAs(klines, ma=30)
+            MAs = calcMAs(KLines, ma=30)
             _, ma = MAs[-1]
             last_price = ticker[-4]
             print('last_price && ma:', last_price, ma)
+            if last_price > ma and len(buys) == 0:
+                print('BUY')
+            elif last_price < ma and len(sells) == 0:
+                print('SELL')
+
+        if not catch and KLines is not None and ticker is not None:
+            Bolls = calcBolls(KLines)
+            klines = KLines[-3:]
+            bolls = Bolls[-3:]
+            llk, lk, k = klines
+            llb, lb, b = bolls
+            _, _, _, _, llk_close, _, _ = llk
+            _, _, _, _, lk_close, _, _ = lk
+            _, llb_u, _, llb_d = llb
+            _, lb_u, _, lb_d = lb
+            _, b_u, _, b_d = b
+            if llk_close > llb_d and lk_close < lb_d:
+                print('BUYBOLL')
+            elif llk_close < llb_u and lk_close > lb_u:
+                print('SELLBOLL')
+
+        if not catch:
+            buyp = self.data['buyp'] # 未成交的卖单，相当于平多单
+            sellp = self.data['sellp'] # 未成交的买单，相当于平空单
+            if buyp is not None:
+                buyp['check'] = False
+                print('CHECK BUYP')
+                self.state = 'wait_for_check'
+            if sellp is not None:
+                sellp['check'] = False
+                print('CHECK SELLP')
+                self.state = 'wait_for_check'
+
+
+
+    def wait_for_check(self):
+        buyp = self.data['buyp']
+        sellp = self.data['sellp']
+        if buyp['check'] and sellp['check']:
+            self.state = 'run'
+
 
 pairs = (coin, money)
 
@@ -38,7 +90,7 @@ klinesCycle.start(pairs, last=30)
 tickerCycle = Cycle(reactor, bitfinex.getTicker, 'ticker', limit=1, wait=3, clean=False)
 tickerCycle.start(pairs)
 
-states = ['run']
+states = ['init', 'run', 'wait_for_check']
 
 bitfinexRobot = BitfinexRobot(reactor, states, [klinesCycle, tickerCycle])
-bitfinexRobot.start('run')
+bitfinexRobot.start('init')
