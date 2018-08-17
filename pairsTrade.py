40bab1809e0ae9f61e506a9b3c1affa2f5595b01
CanDoge <172975487@qq.com>
2018-08-17 23:11:23 +0800

    finish pairsTrade.py with once checkout and no test && clean codes

--- pairsTrade.py
+++ pairsTrade.py
@@ -6,7 +6,8 @@ import time
 from twisted.internet import defer, task
 from twisted.internet import reactor
 from twisted.python.failure import Failure
-from requestUtils.request import get
+
+
 from utils import calcMean, getLevel
 from exchange import calcVirtualOrderBooks, verifyExchanges
 from exchanges.gateio.GateIOService import gateio
@@ -18,7 +19,10 @@ from cycle.cycle import Cycle
 
 count = 0
 '''initial OrderBooks'''
-coinPair1 = ('eth', 'usdt') #1 2 →3
+coinA = 'usdt'  #A2C, C2B -> A2B
+coinC = 'eth'
+coinB = 'eos'
+coinPair1 = ('eth', 'usdt')  #1 2 ->3
 coinPair2 = ('eos', 'eth')
 coinPair3 = ('eos', 'usdt')
 exchangeName = 'bitfinex'
@@ -67,7 +71,7 @@ def buy(exchange,coinPair,amount,price):
             failure = Failure(err)
             print(failure.getBriefTraceback())
 
-#    state = "GO"
+   state = "GO"
 
 @defer.inlineCallbacks
 def sell(exchange,coinPair,amount,price):
@@ -137,12 +141,11 @@ def cbRun():
 
             exchangeState[exchange]['actual'], exchangeState[exchange]['avg'] = [bids, asks], [avgBids, avgAsks]
 
-        # print(exchangeState)
-
         if hasData:
             '''add virtualOrderBook into exchangeSate '''
-            virtualOrderBooks = calcVirtualOrderBooks(A, B)  
-            # print(count, virtualOrderBooks)
+            virtualOrderBooks = calcVirtualOrderBooks(A, B)
+            print('midAmount in virtualOrderBooks:')  
+            print(count, virtualOrderBooks[1])
             vBUY, vSELL = range(2)
             virBids = virtualOrderBooks[0][vBUY]
             virAsks = virtualOrderBooks[0][vSELL]
@@ -156,23 +159,13 @@ def cbRun():
             
             '''get validExPairs '''
             exchangePairs = verifyExchanges(exchangeState)
+            print('validExPairs:')
             print(count, exchangePairs)
 
-            '''possible approaches to get price and amount:
-            
-            midAmount = virtualOrderBooks['midAmount']
-            amountA = midAmount
-            levelA = getLevel(amountA, A[0or1])
-            priceA = A[0or1][levelA][PRICE]
-            
-            amountB = exchangePairs[0][2][1]
-            levelB = getLevel(amountB, B[0or1])
-            priceB = B[0or1][levelB][PRICE]
             '''
-
-            '''
-            buy and sell 
+            buy and sell:
             BUY=1, SELL=0
+            PRICE=0, AMOUNT=1
             '''
             if exchangePairs:  #skip when exchangePairs is [] or None.
                 state = "WAIT"
@@ -190,12 +183,35 @@ def cbRun():
                     balanceC = balances[coinC]  #balance of 'eth'
                     balanceB = balances[coinB]  #balance of 'eos'
                 exchange = EXCHANGE[exchangeName]  #original exchange instance, eg bitfinex
-                '''do buy and sell '''
+                
+                '''do buy '''
                 print('do: BUY')
                 strExchange = exchangePairs[0][0][BUY]
+
+                '''do buy in two trade districts:'''
                 if strExchange == 'virtual':
-                    exchange = EXCHANGE[exchangePairs[0][0][BUY]]
-                    #TO DO 
+                    #first, in orderBookA: eth-usdt
+                    amountBuyA = midAmountBuy
+                    levelA = getLevel(amountBuyA,A[BUY])
+                    priceBuyA = A[BUY][levelA][PRICE]                    
+                    #second, in orderBookB: eos-eth
+                    amountBuyB = exchangePairs[0][2][1]  
+                    levelB = getLevel(amountBuyB,B[BUY])
+                    priceBuyB = B[BUY][levelB][PRICE]
+
+                    if isinstance(balanceA,float) and isinstance(balanceC,float):
+                        if amountBuyA*priceBuyA <= balanceA and amountBuyB*priceBuyB <= balanceC:
+                            print(balances)
+                            reactor.callWhenRunning(buy,exchange=exchange,coinPair=orderBooksA.pairs,price=priceBuyA,amount=amountBuyA)
+                            reactor.callWhenRunning(buy,exchange=exchange,coinPair=orderBooksB.pairs,price=priceBuyB,amount=amountBuyB)
+                        else:
+                            state = "GO"
+                            print("Not enough coin/money")
+                    else:
+                        state = "GO"
+                        print("No exchange")  
+                    
+                '''do buy in real district:orderBooks: eos-usdt'''
                 else:
                     priceBuy = exchangePairs[0][1][BUY]
                     amountBuy = exchangePairs[0][2][1]
@@ -203,6 +219,7 @@ def cbRun():
                     print(type(amountBuy))
                     if isinstance(balanceA,float):
                         if amountBuy*priceBuy <= balanceA:
+                            print(balances)
                             reactor.callWhenRunning(buy,exchange=exchange,coinPair=orderBooks.pairs,price=priceBuy,amount=amountBuy)
                         else:
                             state = "GO"
@@ -211,11 +228,50 @@ def cbRun():
                         state = "GO"
                         print("No exchange")
 
+                
+                '''do sell '''
                 print('do: SELL')
                 strExchange = exchangePairs[0][0][SELL]
+
+                '''do sell in two trade districts:'''
                 if strExchange == 'virtual':
-                    exchange = EXCHANGE[exchangePairs[0][0][BUY]]
-                    #TO DO
+                    #first, in orderBookB: eos-eth
+                    amountSellB = exchangePairs[0][2][1]  
+                    levelB = getLevel(amountSellB,B[SELL])
+                    priceSellB = B[SELL][levelB][PRICE]
+                    #second, in orderBookA: eth-usdt
+                    amountSellA = midAmountSell
+                    levelA = getLevel(amountSellA,A[SELL])
+                    priceSellA = A[SELL][levelA][PRICE]
+
+                    if isinstance(balanceB,float) and isinstance(balanceC,float):
+                        if amountSellB <= balanceB and amountSellA <= balanceC:
+                            print(balances)
+                            reactor.callWhenRunning(sell,exchange=exchange,coinPair=orderBooksB.pairs,price=priceSellB,amount=amountSellB)
+                            reactor.callWhenRunning(sell,exchange=exchange,coinPair=orderBooksA.pairs,price=priceSellA,amount=amountSellA)
+                        else:
+                            state = "GO"
+                            print("Not enough coin/money")
+                    else:
+                        state = "GO"
+                        print("No exchange")                   
+
+                '''do sell in real district:orderBooks: eos-usdt'''
+                else:
+                    priceSell = exchangePairs[0][1][SELL]
+                    amountSell = exchangePairs[0][2][1]
+                    print(type(priceSell))
+                    print(type(amountSell))
+                    if isinstance(balanceB,float):
+                        if amountSell*priceSell <= balanceB:
+                            print(balances)
+                            reactor.callWhenRunning(sell,exchange=exchange,coinPair=orderBooks.pairs,price=priceSell,amount=amountSell)
+                        else:
+                            state = "GO"
+                            print("Not enough coin/money")
+                    else:
+                        state = "GO"
+                        print("No exchange")
 
 
     # yield cbRun()
@@ -227,10 +283,8 @@ def ebLoopFailed(failure):
     reactor.stop()
 
 # reactor.callWhenRunning(cbRun)
-coinA = 'usdt'
-coinC = 'eth'
-coinB = 'eos'
-coinList = [coinA, coinC, coinB]  #A,C,B .A2C,C2B -- A2B
+
+coinList = [coinA, coinC, coinB]  #A,C,B | A2C,C2B -> A2B
 coinPair = ('usdt', 'eth')
 HuobiBalancesCycle = Cycle(reactor,huobipro.getBalances,'balances')
 HuobiBalancesCycle.start(list(coinPair))
