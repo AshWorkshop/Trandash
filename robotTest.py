d06b0a89ddf3f1321c7df8f859014a457bcd735b
Nouzan <clobraindie@outlook.com>
2018-08-25 19:03:22 +0800

    add system event && make stopListion possiable

--- robotTest.py
+++ robotTest.py
@@ -8,19 +8,24 @@ def counter():
 class TestRobot(RobotBase):
     def launch(self, oldState, newState):
         actions = []
-        print(newState)
-        # if newState['gateio']['orderbook'] is not None:
-        #     action = Action(reactor, gateio.buy, payload={
-        #         'args': [('eth', 'usdt'), 2, 1]
-        #     })
-        #     actions.append(action)
+        print(newState.get('count'))
+        if newState['count'] == 10 and newState.get('tickSource') is not None:
+            print('STOP LISTEN TICKEVENT')
+            self.stopListen(newState['tickSource'])
+            # newState['tickSource'].stop()
+        if newState['count'] == 15 and newState.get('tickSource') is None:
+            print('START LISTEN TICKEVENT')
+            self.listen(newState['tickBackup'])
         return actions
 
     def gateioOrderBookHandler(self, state, dataRecivedEvent):
         newState = dict()
         newState.update(state)
-        newState['gateio'] = state.get('gateio', dict())
+        newState['gateio'] = dict()
+        newState['gateio'].update(state.get('gateio', dict()))
         newState['gateio']['orderbook'] = dataRecivedEvent.data['data']
+        if newState.get('count', 0) == 10:
+            newState['count'] += 5
 
         return newState
 
@@ -29,6 +34,20 @@ class TestRobot(RobotBase):
         newState.update(state)
         newState['count'] = state.get('count', 0) + 1
 
+        return newState
+
+    def systemEventHandler(self, state, systemEvent):
+        newState = dict()
+        newState.update(state)
+        if systemEvent.data['type'] == 'LISTEN_STOPPED':
+            if systemEvent.data['info']['source'] == state['tickSource']:
+                newState['tickSource'] = None
+                newState['tickBackup'] = systemEvent.data['info']['source']
+        elif systemEvent.data['type'] == 'LISTEN_STARTED':
+            if systemEvent.data['info']['source'] is not None and systemEvent.data['info']['source'] == state.get('tickBackup'):
+                newState['tickSource'] = systemEvent.data['info']['source']
+                newState['tickBackup'] = None
+    
         return newState
 
 gateioSource = CycleSource(reactor, gateio.getOrderBook, key='gateio', payload={
@@ -51,6 +70,11 @@ robot.bind(
     'tickEvent',
     robot.tickHandler
 )
+
+robot.state.update({
+    'tickSource': tickSource,
+})
+
 gateioSource.start()
 tickSource.start()
 reactor.run()
